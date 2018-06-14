691b69aae252c343838505486367ed757cdc33eb
Thomas Ferro <t.ferro184@gmail.com>
2018-06-14 21:46:58 +0200

    Core (repositories) : Update in memory user repository

--- src/infra/persistence/in-memory/UserRepository.ts
+++ src/infra/persistence/in-memory/UserRepository.ts
@@ -8,7 +8,7 @@ export class InMemoryUserRepository implements IUserRepository {
 
     constructor() {
         this.users = [];
-    }
+    };
     
     findUserById(userId: string): User {
         if (!userId) {
@@ -19,5 +19,47 @@ export class InMemoryUserRepository implements IUserRepository {
             return user &&
                 user.userId === userId;
         });
+    };
+
+    searchUsers(query: string): Array<User> {
+        if (!query) {
+            return [];
+        }
+        
+        return this.users.filter((user: User) => {
+            return user &&
+                (user.name && user.name.toLowerCase().indexOf(query)) ||
+                (user.email && user.email.toLowerCase().indexOf(query));
+        });
+    };
+
+    createUser(user: User): User {
+        const createdUser = new User();
+        createdUser.name = user.name;
+        createdUser.login = user.login;
+        createdUser.email = user.email;
+        createdUser.userId = this.generateGuid();
+        this.users.push(createdUser);
+        return createdUser;
+    };
+
+    deleteUser(userId: string): boolean {
+        let success: boolean = false;
+        const userIndex = this.users.findIndex((user: User): boolean => {
+            return user && user.userId === userId;
+        });
+        if (userIndex >= 0) {
+            success = !!this.users.splice(userIndex, 1);
+        }
+        return success;
+    };
+
+    private generateGuid(): string {
+        const s4 = (): string => {
+            return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 };
